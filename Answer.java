c91855ccfd23ee77117bee208c28869ae16940b5
Arnav Limaye <91765427+AstronomicalWind@users.noreply.github.com>
2025-02-14 22:45:53 -0700

    Update Answer.java

--- Answer.java
+++ Answer.java
@@ -1,12 +1,13 @@
 package application;
 
 public class Answer {
-
+	//Sets up the answer propertiers
 	public String id;
 	public int number;
 	public String answer;
 	public boolean resolved;
 	public String answerNum;
+	//constructor to quickly fill it in
 	public Answer(int num, String id, String answerText) {
 		// TODO Auto-generated constructor stub
 		
