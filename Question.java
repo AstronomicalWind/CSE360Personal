43dbc0fb9629e9c6b4d72146d567b8e4abefad7e
Arnav Limaye <91765427+AstronomicalWind@users.noreply.github.com>
2025-02-14 22:40:21 -0700

    Update Question.java

--- Question.java
+++ Question.java
@@ -11,26 +11,27 @@ public class Question {
 	public boolean resolved=false;
 	public boolean answered=false;
 	List<Answer> answerList = new ArrayList<>();
-	//public static int numCounter=0;
-
+	//these are attributes of the queston class
 	
 	public Question(String questionId, String questionText, String description, String numb) {
 		// TODO Auto-generated constructor stub
-		
+		//This constructor was auto made in eclipse and allows us to quickly fill in the values from the main file
 		 this.id = questionId;
 	        this.question = questionText;
 	        this.description = description;
 	        this.answered = false;
 	        this.resolved=false;
 	        this.number=numb;
-	        //numCounter+=1;
+	        
 	}
 	
 	public void addAnswer(Answer answer) {
         answerList.add(answer);
         answered = true; // Mark the question as answered
     }
-	
+
+
+	//OLD CODE IGNORE - KEPT FOR STORAGE PURPOSES
 	/*
 	 * public void displayAnswers() { if (answerList.isEmpty()) {
 	 * System.out.println("No answers");
